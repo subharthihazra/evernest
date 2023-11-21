5c0567eb32b59989487725ebd8c147acc8b6501f
subharthi75 <subharthi75@gmail.com>
2023-11-21 12:22:39 +0530

    setup init

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+const port = process.env.SERVER_PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port} ...`);
+});
