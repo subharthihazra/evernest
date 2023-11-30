85a383500cceb1020602757593cc0126fc2bc24e
subharthi75 <subharthi75@gmail.com>
2023-11-30 18:42:39 +0530

    menu.js added

---
+++ frontend/src/data/menu.js
@@ -0,0 +1,40 @@
+export default [
+  {
+    trigger: "MEN",
+    content: [
+      "Printed Tshirts",
+      "Oversized Tshirts",
+      "Full Sleeve Tshirts",
+      "Half Sleeve Tshirts",
+      "Polo Tshirts",
+      "Sweatshirts",
+      "Shirts",
+      "Joggers",
+    ],
+  },
+  {
+    trigger: "WOMEN",
+    content: [
+      "Printed Tshirts",
+      "Oversized Tshirts",
+      "Full Sleeve Tshirts",
+      "Half Sleeve Tshirts",
+      "Polo Tshirts",
+      "Sweatshirts",
+      "Shirts",
+      "Joggers",
+    ],
+  },
+  {
+    trigger: "KIDS",
+    content: [
+      "Printed Tshirts",
+      "Full Sleeve Tshirts",
+      "Half Sleeve Tshirts",
+      "Shirts",
+      "Joggers",
+      "Trousers",
+      "Toys",
+    ],
+  },
+];
