e1399015ac528c65b9d5bef339fcfc4777ed8d15
subharthi75 <subharthi75@gmail.com>
2024-01-10 00:15:34 +0530

    user redux

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./cart";
+import userReducer from "./user";
 
 const store = configureStore({
   reducer: {
     cart: cartReducer,
+    user: userReducer,
   },
 });
 
