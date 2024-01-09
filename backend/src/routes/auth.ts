1a4ede2439a09f4684b2705b25f683a9fa5d1f7b
subharthi75 <subharthi75@gmail.com>
2024-01-10 00:14:51 +0530

    auth

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,11 +1,15 @@
 import { Express, Router } from "express";
-import { signup, signin, logout } from "../controllers/auth";
-import verifyUserMidddleware from "../validations/verifyUserMidddleware";
+import { signup, signin, isAuth, logout } from "../controllers/auth";
+import {
+  verifyUserMiddleware,
+  isUserMiddleware,
+} from "../validations/verifyUserMiddleware";
 
 const router = Router();
 
-router.post("/signup", signup);
-router.post("/signin", signin);
-router.delete("/logout", verifyUserMidddleware, logout);
+router.post("/signup", isUserMiddleware, signup);
+router.post("/signin", isUserMiddleware, signin);
+router.post("/isauth", verifyUserMiddleware, isAuth);
+router.post("/logout", verifyUserMiddleware, logout);
 
 export default router;
